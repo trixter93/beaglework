69a7474cd0c237494d2a06efff191e127a6fe446
nabila-mwicom <farahbella559@gmail.com>
2016-08-18 14:36:33 +0800

    Create LocomotiveController.h

---
+++ LocomotiveController/LocomotiveController.h
@@ -0,0 +1,35 @@
+/*
+ * LocomotiveController.h
+ *
+ *  Created on: Aug 15, 2016
+ *      Author: pc2
+ */
+
+#ifndef LOCOMOTIVECONTROLLER_H_
+#define LOCOMOTIVECONTROLLER_H_
+
+#include <iostream>
+#include "BlackGPIO/BlackGPIO.h"
+#include <string>
+#include <time.h>
+#include <sys/time.h>
+
+using namespace std;
+using namespace BlackLib;
+
+class LocomotiveController {
+public:
+
+	LocomotiveController();
+	virtual ~LocomotiveController();
+	float getStatus(); //give status of the driver; return speed of wheel.
+	int getStatusOverSpeed(); // give the status of the speed; 1=over-speed, 0=NONoverspeed
+
+
+private:
+    BlackGPIO *SPEEDpin;
+
+};
+
+
+#endif /* LOCOMOTIVECONTROLLER_H_ */
